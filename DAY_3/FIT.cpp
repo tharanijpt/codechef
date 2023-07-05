6f575bbf01873b85382cfb14fda76fb7486d7f99
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-05 19:07:43 +0530

    Create FIT.cpp

---
+++ DAY_3/FIT.cpp
@@ -0,0 +1,15 @@
+//https://www.codechef.com/problems/FIT
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T,distance,total;
+	cin >> T;
+	while(T--){
+	    cin >> distance;
+	    //calculating using give information
+	    total=distance*2*5;
+	    cout<< total <<endl;
+	}
+	return 0;
+}
