8c1b3ad3508ab22ff67722d1cf79089d60b8e75a
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-05 22:52:09 +0530

    Create PRACLIST.cpp

---
+++ DAY_3/PRACLIST.cpp
@@ -0,0 +1,10 @@
+//https://www.codechef.com/problems/PRACLIST
+#include <iostream>
+using namespace std;
+
+int main() {
+    int non_attempted, attempted;
+    cin >> non_attempted >> attempted;
+    cout<< non_attempted-attempted;
+	return 0;
+}
