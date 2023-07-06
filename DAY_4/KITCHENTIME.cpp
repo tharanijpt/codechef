4a83161810b99bcd62eda0c9e5432062f8a8e6e4
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-06 17:44:19 +0530

    Create KITCHENTIME.cpp

---
+++ DAY_4/KITCHENTIME.cpp
@@ -0,0 +1,13 @@
+//https://www.codechef.com/problems/KITCHENTIME
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T,start,end;
+cin >> T;
+while(T--){
+cin>>start>>end;
+    cout<<end-start<<endl;
+}
+	return 0;
+}
