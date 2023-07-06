232a5f997f14b61e509f1ac0760a0ea58fb0c488
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-06 17:43:17 +0530

    Create DONDIVE.cpp

---
+++ DAY_4/DONDIVE.cpp
@@ -0,0 +1,13 @@
+//https://www.codechef.com/problems/DONDRIVE
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T,N,X;
+cin >> T;
+while(T--){
+cin>>N>>X;
+    cout<<N-X<<endl;
+}
+	return 0;
+}
