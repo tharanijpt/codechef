a0edd273a9254b88f9ef8b35bb8f518f0a66fa5c
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-10 17:39:53 +0530

    Create AUDIBLE.cpp

---
+++ DAY_7/AUDIBLE.cpp
@@ -0,0 +1,14 @@
+//https://www.codechef.com/problems/AUDIBLE
+#include <iostream>
+using namespace std;
+
+int main() {
+int T,ferquency;
+cin >> T;
+while(T--){
+cin>>ferquency;
+(67<=ferquency && ferquency<=45000)?cout<<"YES"<<endl: cout<<"NO"<<endl;
+    
+}
+	return 0;
+}
