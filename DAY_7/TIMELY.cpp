b755223364e417328a506654a37dcaaebc0b5b49
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-10 17:38:15 +0530

    Create TIMELY.cpp

---
+++ DAY_7/TIMELY.cpp
@@ -0,0 +1,14 @@
+//https://www.codechef.com/problems/TIMELY
+#include <iostream>
+using namespace std;
+
+int main() {
+int T,time;
+cin >> T;
+while(T--){
+    cin>>time;
+    (time<30)?cout<<"NO"<<endl : cout<<"YES"<<endl;
+    
+}
+	return 0;
+}
