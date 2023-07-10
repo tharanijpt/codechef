284329710ca90928f16d27b16784026c0348d7b9
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-10 17:39:28 +0530

    Create REACHTARGET.cpp

---
+++ DAY_7/REACHTARGET.cpp
@@ -0,0 +1,14 @@
+//https://www.codechef.com/problems/REACHTARGET
+#include <iostream>
+using namespace std;
+
+int main() {
+int T,num1,num2;
+cin >> T;
+while(T--){
+    cin>>num1>>num2;
+    cout<<num1-num2<<endl;
+    
+}
+	return 0;
+}
