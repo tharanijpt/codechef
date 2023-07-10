0c80f685b726705d91d613647df074f79f9bd018
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-10 17:40:15 +0530

    Create KNGTOR.cpp

---
+++ DAY_7/KNGTOR.cpp
@@ -0,0 +1,13 @@
+//https://www.codechef.com/problems/KNGTOR
+#include <iostream>
+using namespace std;
+
+int main() {
+int T,N,M;
+cin >> T;
+while(T--){
+    cin>>N>>M;
+    cout<< N*5 + M*7 <<endl;  
+}
+	return 0;
+}
