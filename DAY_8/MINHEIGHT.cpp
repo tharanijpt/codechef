ef21f757cbcb48bdb70a3565b233dc86b1207ffe
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-11 20:51:47 +0530

    Create MINHEIGHT.cpp

---
+++ DAY_8/MINHEIGHT.cpp
@@ -0,0 +1,14 @@
+//https://www.codechef.com/problems/MINHEIGHT
+#include <iostream>
+using namespace std;
+
+int main() {
+int T,num1,num2;
+cin >> T;
+while(T--){
+    cin >> num1>> num2;
+    (num1>=num2)?cout<<"YES"<<endl:cout<<"NO"<<endl;
+    
+}
+	return 0;
+}
