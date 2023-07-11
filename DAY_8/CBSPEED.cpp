dc21659760d73c78eceb62089db2f511b7d9a0fc
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-11 21:16:12 +0530

    Create CBSPEED.cpp

---
+++ DAY_8/CBSPEED.cpp
@@ -0,0 +1,13 @@
+//https://www.codechef.com/problems/CBSPEED
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+int threshold,speed;
+    
+    cin>>threshold>>speed;
+    (threshold>=speed)?cout<<"NO"<<endl: cout<<"YES"<<endl;
+	
+	return 0;
+}
