795b90f97303addf6db8faa343ecaae786f735b1
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-12 22:39:55 +0530

    Create CANDIVIDE.cpp

---
+++ CANDIVIDE.cpp
@@ -0,0 +1,14 @@
+//https://www.codechef.com/problems/CANDIVIDE
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T,num1;
+    cin >> T;
+while(T--){
+    cin>>num1;
+    (num1%3==0)?cout<<"YES"<<endl : cout<<"NO"<<endl;
+    
+}
+	return 0;
+}
