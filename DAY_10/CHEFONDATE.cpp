89fd140ccd3dc63b2df7045e23377b2f3decd543
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-13 20:20:08 +0530

    Create CHEFONDATE.cpp

---
+++ DAY_10/CHEFONDATE.cpp
@@ -0,0 +1,14 @@
+//https://www.codechef.com/problems/CHEFONDATE
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T,money,bill;
+    cin >> T;
+while(T--){
+    cin>>money>>bill;
+    (money>=bill)?cout<<"YES"<<endl : cout<<"NO"<<endl;
+    
+}
+	return 0;
+}
