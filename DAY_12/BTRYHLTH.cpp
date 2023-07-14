62a379073fab50e55904291fc5dffb070d4d8f9e
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-14 13:56:28 +0530

    Create BTRYHLTH.cpp

---
+++ DAY_12/BTRYHLTH.cpp
@@ -0,0 +1,14 @@
+//https://www.codechef.com/problems/BTRYHLTH
+#include <iostream>
+using namespace std;
+
+int main() {
+int T,battery;
+cin >> T;
+while(T--){
+    cin>>battery;
+    (battery>=80)?cout<<"YES"<<endl:cout<<"NO"<<endl;
+    
+}
+	return 0;
+}
