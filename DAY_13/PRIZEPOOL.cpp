599be3358cdcfc89d40f0220306c25917bac848a
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-14 14:00:33 +0530

    Create PRIZEPOOL.cpp

---
+++ DAY_13/PRIZEPOOL.cpp
@@ -0,0 +1,14 @@
+//https://www.codechef.com/problems/PRIZEPOOL
+#include <iostream>
+using namespace std;
+
+int main() {
+int T,top,others;
+cin >> T;
+while(T--){
+    cin>>top>>others;
+    cout<<10*top+90*others<<endl;
+    
+}
+	return 0;
+}
