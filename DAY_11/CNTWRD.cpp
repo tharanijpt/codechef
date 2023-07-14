98eba076e68cb22049933da8e30f9bfb38885c1d
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-14 13:53:16 +0530

    Create CNTWRD.cpp

---
+++ DAY_11/CNTWRD.cpp
@@ -0,0 +1,14 @@
+//https://www.codechef.com/problems/CNTWRD
+#include <iostream>
+using namespace std;
+
+int main() {
+int T,pages,words;
+cin >> T;
+while(T--){
+    cin>>pages>>words;
+    cout<<pages*words<<endl;
+    
+}
+	return 0;
+}
