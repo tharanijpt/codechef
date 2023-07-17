83f450ce8fe1bc695cbda1ddb682ab09587be619
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-17 21:17:58 +0530

    Create AGEING.cpp

---
+++ DAY_13/AGEING.cpp
@@ -0,0 +1,15 @@
+
+//https://www.codechef.com/problems/AGEING
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T,chef_age;
+    cin >> T;
+while(T--){
+    cin >>chef_age;
+    cout<< chef_age-10 <<endl;
+    
+}
+	return 0;
+}
