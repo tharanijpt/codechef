49623d3d28099aa621a240938224dee6dd3cfa5e
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-17 21:17:36 +0530

    Create JERRYCHASE.cpp

---
+++ DAY_13/JERRYCHASE.cpp
@@ -0,0 +1,15 @@
+//https://www.codechef.com/problems/JERRYCHASE
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T,jerry,tom;
+    cin >> T;
+while(T--){
+    cin>>jerry>>tom;
+    (jerry<tom)?cout<<"yes"<<endl:cout<<"no"<<endl;
+    
+}
+	return 0;
+}
