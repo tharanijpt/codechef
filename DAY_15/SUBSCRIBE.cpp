5caad9d68d177794b980bd5ec63b4f53955a38ce
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-18 21:25:05 +0530

    Create SUBSCRIBE.cpp

---
+++ DAY_15/SUBSCRIBE.cpp
@@ -0,0 +1,13 @@
+//https://www.codechef.com/problems/SUBSCRIBE
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T,time;
+	cin >> T;
+while(T--){
+    cin>> time;
+(time>30)?cout<<"YES"<<endl:cout<<"NO"<<endl;
+}
+	return 0;
+}
