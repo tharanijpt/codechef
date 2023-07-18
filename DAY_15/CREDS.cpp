b5188bac06de72b9fe426542d249e47aedaa2c9a
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-18 21:24:16 +0530

    Create CREDS.cpp

---
+++ DAY_15/CREDS.cpp
@@ -0,0 +1,14 @@
+//https://www.codechef.com/problems/CREDS
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T,rtp,audit,non_rtp;
+	cin >> T;
+while(T--){
+    
+    cin >> rtp>>audit>>non_rtp;
+    cout << rtp*4 + audit*2 + non_rtp*0 << endl;
+}
+	return 0;
+}
