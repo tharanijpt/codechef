d375ad8322ef3188780d33c720c6056ee35ba2d4
tharanijpt <tharanijpt@gmail.com>
2023-07-18 21:16:44 +0530

    Create FOURTICKETS.cpp

---
+++ DAY_15/FOURTICKETS.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T,cost;
+	cin >> T;
+while(T--){
+    cin >> cost;
+    (cost*4<=1000)?cout<<"YES"<<endl:cout<<"NO"<<endl;
+}
+	return 0;
+}
