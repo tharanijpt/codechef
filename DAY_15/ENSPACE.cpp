d863787f59c1b64b8e9074acdcabf1ba55c812c8
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-18 21:25:49 +0530

    Create ENSPACE.cpp

---
+++ DAY_15/ENSPACE.cpp
@@ -0,0 +1,13 @@
+//https://www.codechef.com/problems/ENSPACE
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T,N,X,Y;
+	cin >> T;
+while(T--){
+cin >> N >> X >> Y;
+(X+Y*2<=N) ?cout<<"YES"<<endl:cout<<"NO"<<endl;
+}
+	return 0;
+}
