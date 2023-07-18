d024fcec9404af1a6a261d7246ef46d548b48247
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-18 21:28:24 +0530

    Create WAITTIME.cpp

---
+++ DAY_15/WAITTIME.cpp
@@ -0,0 +1,13 @@
+//https://www.codechef.com/problems/WAITTIME
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T,K,X;
+	cin >> T;
+while(T--){
+cin >> K >> X;
+cout << K*7 - X << endl;
+}
+	return 0;
+}
