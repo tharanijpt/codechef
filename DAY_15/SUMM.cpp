d71767b2413a70299833d98dfea443d9a9e068e7
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-18 21:24:41 +0530

    Create SUMM.cpp

---
+++ DAY_15/SUMM.cpp
@@ -0,0 +1,13 @@
+//https://www.codechef.com/problems/SUMM
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T,A,B,C;
+	cin >> T;
+while(T--){
+    cin>>A>>B>>C;
+    (A+B==C)?cout<<"YES"<<endl:cout<<"NO"<<endl;
+}
+	return 0;
+}
