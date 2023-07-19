cf2013c588068b338df8e1eeeca6b68f3660edd9
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-19 18:55:57 +0530

    commit message

---
+++ DAY_16/ONEMORE.cpp
@@ -0,0 +1,12 @@
+\\https://www.codechef.com/problems/ONEMORE
+#include <iostream>
+using namespace std;
+int main() {
+	int T,exam_time;
+	cin >> T;
+while(T--){
+cin>>exam_time;
+(exam_time>24) ?cout<<"YES"<<endl:cout<<"NO"<<endl;
+}
+	return 0;
+}
