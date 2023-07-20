0ce75cb9a395e93e4f8ea274301e799a89ab585a
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-20 20:08:49 +0530

    Create PUZHUNT.cpp

---
+++ DAY_16/PUZHUNT.cpp
@@ -0,0 +1,10 @@
+//https://www.codechef.com/problems/PUZHUNT
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num;
+	cin >> num;
+    (6<=num && 8>=num)? cout<<"Yes"<<endl : cout<<"No"<<endl;
+	return 0;
+}
