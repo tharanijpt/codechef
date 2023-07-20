ec370c73729c3f673c14f98abffda024b4e0067f
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-20 20:12:00 +0530

    Create TASTEDEC.cpp

---
+++ DAY_16/TASTEDEC.cpp
@@ -0,0 +1,17 @@
+//https://www.codechef.com/problems/TASTEDEC
+#include <iostream>
+using namespace std;
+int main() {
+	int T,choco,candy;
+	cin >> T;
+while(T--){
+    cin >> choco >> candy;
+if (choco*2 > candy*5)
+    cout <<"Chocolate"<< endl;
+else if (choco*2 < candy*5)
+    cout <<"Candy" << endl;
+else
+    cout <<"Either"<< endl;
+}
+	return 0;
+}
