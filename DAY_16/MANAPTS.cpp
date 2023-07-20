528e5daf4ec3292ff05a0966176e87e8dcf0cc1f
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-20 20:18:13 +0530

    Create MANAPTS.cpp

---
+++ DAY_16/MANAPTS.cpp
@@ -0,0 +1,13 @@
+//https://www.codechef.com/problems/MANAPTS
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T,X,Y;
+	cin >> T;
+while(T--){
+    cin >> X >> Y;
+    cout <<Y/X << endl;
+}
+	return 0;
+}
