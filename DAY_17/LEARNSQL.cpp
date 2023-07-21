514a9324bd2c499939a77671a6e23b613989ce94
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-21 20:46:24 +0530

    Create LEARNSQL.cpp

---
+++ DAY_17/LEARNSQL.cpp
@@ -0,0 +1,10 @@
+//https://www.codechef.com/problems/LEARNSQL
+#include <iostream>
+using namespace std;
+int main() {
+	int R,C,E;
+cin >> R >> C >> E;
+    cout << (E+R)*C << endl;
+
+	return 0;
+}
