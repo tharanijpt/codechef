024f260bb664bbed2d0c6c15225a3e920d0efa6b
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-21 22:10:48 +0530

    Create COUGAME.cpp

---
+++ DAY_17/COUGAME.cpp
@@ -0,0 +1,13 @@
+//https://www.codechef.com/problems/COUGAME
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T,G,B;
+	cin >> T;
+while(T--){
+    cin >> G >> B;
+    cout << B-G << endl;
+}
+	return 0;
+}
