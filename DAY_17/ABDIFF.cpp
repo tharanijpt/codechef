5e87119a78dc877825eabdb5fdd590617f4c8c7d
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-21 22:18:58 +0530

    Create ABDIFF.cpp

---
+++ DAY_17/ABDIFF.cpp
@@ -0,0 +1,13 @@
+//https://www.codechef.com/problems/ABDIFF
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int X,Y;
+    cin >> X >> Y;
+
+    cout << abs((X*Y)-(X+Y))<< endl;
+	return 0;
+}
+
