5a3f85a44159515a2132c6fa9194dc3adb69692b
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-21 22:20:45 +0530

    Create FEVER.cpp

---
+++ DAY_17/FEVER.cpp
@@ -0,0 +1,13 @@
+//https://www.codechef.com/problems/FEVER
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T,temp;
+	cin >> T;
+while(T--){
+    cin >> temp;
+    (temp>98)?cout<<"YES"<<endl:cout<<"NO"<<endl;
+}
+	return 0;
+}
