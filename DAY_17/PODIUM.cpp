a270b5fbc8932c871980b17ed84e70616fbae940
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-21 20:48:40 +0530

    Create PODIUM.cpp

---
+++ DAY_17/PODIUM.cpp
@@ -0,0 +1,13 @@
+https://www.codechef.com/problems/PODIUM
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T,A,B;
+	cin >> T;
+while(T--){
+    cin >> A >> B;
+    cout << A+ B<< endl;
+}
+	return 0;
+}
