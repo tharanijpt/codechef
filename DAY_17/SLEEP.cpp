2ef4d6df43e845b7efb846786412ebfdab36f0eb
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-21 22:22:53 +0530

    Create SLEEP.cpp

---
+++ DAY_17/SLEEP.cpp
@@ -0,0 +1,13 @@
+//https://www.codechef.com/problems/SLEEP
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T,sleep;
+	cin >> T;
+while(T--){
+      cin >> sleep;
+      (sleep<7)?cout<<"YES"<<endl:cout<<"NO"<<endl;
+}
+	return 0;
+}
