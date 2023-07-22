57ab033539ade8eb7a7cbe91f15a4e8cae9cda8f
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-22 22:07:29 +0530

    Create COMPLEXITY.cpp

---
+++ DAY_18/COMPLEXITY.cpp
@@ -0,0 +1,16 @@
+//https://www.codechef.com/problems/COMPLEXITY
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T,A,B;
+	cin >> T;
+while(T--){
+    cin >> A >> B;
+if (A>B)
+    cout << "YES"<< endl;
+else
+    cout <<"NO" << endl;
+}
+	return 0;
+}
