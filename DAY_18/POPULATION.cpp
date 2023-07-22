81a26f3d027b89fe63f3bf7c7d3cc280a6759719
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-22 21:59:24 +0530

    Create POPULATION.cpp

---
+++ DAY_18/POPULATION.cpp
@@ -0,0 +1,13 @@
+//https://www.codechef.com/problems/POPULATION
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T,X,Y,Z;
+	cin >> T;
+while(T--){
+    cin >>X >> Y>> Z;
+    cout << X-Y+Z<< endl;
+}
+	return 0;
+}
