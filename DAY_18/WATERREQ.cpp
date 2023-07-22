1d297a32b2933bd80f864fe78766f58bffe9c423
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-22 21:46:36 +0530

    Create WATERREQ.cpp

---
+++ DAY_18/WATERREQ.cpp
@@ -0,0 +1,13 @@
+//https://www.codechef.com/problems/WATERREQ
+#include <iostream>
+using namespace std;
+
+int main() {
+    	int T,hour;
+	cin >> T;
+while(T--){
+cin >> hour;
+cout <<hour*2 << endl;
+}
+	return 0;
+}
