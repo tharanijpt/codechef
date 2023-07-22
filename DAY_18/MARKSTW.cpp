fa3ebcb21e8a829b1ac956d209ce5843dfae26fe
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-22 22:01:32 +0530

    Create MARKSTW.cpp

---
+++ DAY_18/MARKSTW.cpp
@@ -0,0 +1,10 @@
+//https://www.codechef.com/problems/MARKSTW
+#include <iostream>
+using namespace std;
+
+int main() {
+int alice,bob;
+cin >> alice >> bob;
+(alice>= bob*2)?cout<<"YES"<<endl:cout<<"NO"<<endl;
+	return 0;
+}
