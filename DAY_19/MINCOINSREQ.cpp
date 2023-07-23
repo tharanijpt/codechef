a44cd312e5fc37fe757f3e27369f1c3f6bfd8df9
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-23 09:22:47 +0530

    Create MINCOINSREQ.cpp

---
+++ DAY_19/MINCOINSREQ.cpp
@@ -0,0 +1,13 @@
+//https://www.codechef.com/problems/MINCOINSREQ
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T,ruppees;
+	cin >> T;
+while(T--){
+    cin >> ruppees;
+    cout << ruppees%10<< endl;
+}
+	return 0;
+}
