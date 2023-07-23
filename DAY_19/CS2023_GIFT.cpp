7262c78e39a03aa1a7cf5360ecb059b0ab37e275
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-23 09:28:50 +0530

    Create CS2023_GIFT.cpp

---
+++ DAY_19/CS2023_GIFT.cpp
@@ -0,0 +1,10 @@
+//https://www.codechef.com/problems/CS2023_GIFT
+#include <iostream>
+using namespace std;
+
+int main() {
+    int amount,laptop,fund;
+    cin >> amount>> laptop>> fund;
+    (amount+fund>=laptop)?cout<<"YES"<<endl:cout<<"NO"<<endl;
+	return 0;
+}
