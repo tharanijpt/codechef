e5a6b8dbbb2f604875e4492d2e4357c9d76cde8e
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-23 09:12:00 +0530

    Create EMAILREM.cpp

---
+++ DAY_19/EMAILREM.cpp
@@ -0,0 +1,11 @@
+//https://www.codechef.com/problems/EMAILREM
+#include <iostream>
+using namespace std;
+
+int main() {
+int users,not_intrested;
+cin >> users >> not_intrested;
+cout << users - not_intrested<< endl;
+	return 0;
+}
+
