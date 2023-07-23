8eb30f1b51508d2da5a36ac4656a04f2dff79129
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-23 09:07:35 +0530

    Create ONEFULPAIRS.cpp

---
+++ DAY_19/ONEFULPAIRS.cpp
@@ -0,0 +1,10 @@
+//https://www.codechef.com/problems/ONEFULPAIRS
+#include <iostream>
+using namespace std;
+
+int main() {
+int a,b;
+cin >> a >> b;
+(a+b+(a*b)==111)?cout<<"YES"<<endl:cout<<"NO"<<endl;
+	return 0;
+}
