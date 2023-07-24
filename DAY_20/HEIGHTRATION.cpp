22dca5aaff3e4aeeead238f2eb918faf3d5d5cb6
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-24 20:14:59 +0530

    Create HEIGHTRATION.cpp

---
+++ DAY_20/HEIGHTRATION.cpp
@@ -0,0 +1,15 @@
+//https://www.codechef.com/problems/HEIGHTRATION
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+int num1,num2;
+
+cin >> num1 >> num2;
+
+int height = max(num1,num2);
+
+cout << height << endl;
+	return 0;
+}
