a30da081c389580057441b96226ce4c9af48724b
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-24 20:35:05 +0530

    Create HOTCOLD.cpp

---
+++ DAY_20/HOTCOLD.cpp
@@ -0,0 +1,13 @@
+//https://www.codechef.com/problems/HOTCOLD
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T,whether;
+	cin >> T;
+while(T--){
+ cin >> whether;
+ (whether>20)?cout<<"HOT"<<endl:cout<<"COLD"<<endl;
+}
+	return 0;
+}
