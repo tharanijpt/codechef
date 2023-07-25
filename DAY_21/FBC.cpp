4824290bb5e3aa9a3675e65389a3e36e8fedd46d
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-25 22:12:28 +0530

    Create FBC.cpp

---
+++ DAY_21/FBC.cpp
@@ -0,0 +1,13 @@
+//https://www.codechef.com/problems/FBC
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T,k,x;
+	cin >> T;
+while(T--){
+    cin >> k >> x;
+    cout << k - x << endl;
+}
+	return 0;
+}
