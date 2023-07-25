2d4c80f4778fe979aae716d16a08ee36b01d7304
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-25 23:04:39 +0530

    Create MANIPULATE.cpp

---
+++ DAY_21/MANIPULATE.cpp
@@ -0,0 +1,18 @@
+//https://www.codechef.com/problems/MANIPULATE
+#include <iostream>
+using namespace std;
+
+int main() {
+ int t, x,y;
+  cin >> t;
+ while(t--){
+     cin >> x >>y;
+     if (x>=y)
+     cout <<"YES" << endl;
+    else 
+    cout << "NO" << endl;
+     
+ }
+	return 0;
+}
+  
