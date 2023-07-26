49bbe9df523dc2d0c71f647361b4efba25aec546
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-26 23:02:09 +0530

    Create BUDTECH.cpp

---
+++ DAY_22/BUDTECH.cpp
@@ -0,0 +1,14 @@
+//https://www.codechef.com/problems/BUDTECH
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T,money;
+	cin >> T;
+while(T--){
+cin >> money;
+int available = money*1000/2;
+cout << available/5<< endl;
+}
+	return 0;
+}
