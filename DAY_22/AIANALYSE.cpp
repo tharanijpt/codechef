df3fa1f9cde07a40f761fc21dd762b58ebb44ee0
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-26 22:55:14 +0530

    Create AIANALYSE.cpp

---
+++ DAY_22/AIANALYSE.cpp
@@ -0,0 +1,10 @@
+//https://www.codechef.com/problems/AIANALYSE
+#include <iostream>
+using namespace std;
+
+int main() {
+int words;
+cin >> words;
+(words<=1000)?cout<<"YES"<<endl:cout<<"NO"<<endl;
+	return 0;
+}
