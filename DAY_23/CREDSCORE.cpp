95e68a4af640dd20731269d9b872ff8362a6083e
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-27 20:51:15 +0530

    Create CREDSCORE.cpp

---
+++ DAY_23/CREDSCORE.cpp
@@ -0,0 +1,10 @@
+//https://www.codechef.com/problems/CREDSCORE
+#include <iostream>
+using namespace std;
+
+int main() {
+int score;
+cin >> score;
+(score>=750)?cout<<"YES"<<endl:cout<<"NO"<<endl;
+	return 0;
+}
