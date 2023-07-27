0cb547ca0b68e7efdfefe959dd7b78982ec28fc7
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-27 20:54:42 +0530

    Create COURSEREG.cpp

---
+++ DAY_23/COURSEREG.cpp
@@ -0,0 +1,13 @@
+//https://www.codechef.com/problems/COURSEREG
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T,n,m,k;
+	cin >> T;
+while(T--){
+cin >> n >>m >>k;
+(n+k<=m)?cout<<"YES"<<endl:cout<<"NO"<<endl;
+}
+	return 0;
+}
