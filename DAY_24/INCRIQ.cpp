21d1d5212f47e77b738dd98a28fff8af24aff43a
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-28 21:46:23 +0530

    Create INCRIQ.cpp

---
+++ DAY_24/INCRIQ.cpp
@@ -0,0 +1,10 @@
+//https://www.codechef.com/problems/INCRIQ
+#include <iostream>
+using namespace std;
+
+int main() {
+int iq;
+cin >> iq;
+(iq + 7 > 170) ? cout<<"YES"<<endl : cout<<"NO"<<endl;
+	return 0;
+}
