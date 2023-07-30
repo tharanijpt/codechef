51b9dd3b8316b0064dd2901fac7e045088dfd74e
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-30 22:24:23 +0530

    Create SHOPCHANGE.cpp

---
+++ DAY_26/SHOPCHANGE.cpp
@@ -0,0 +1,14 @@
+//https://www.codechef.com/problems/SHOPCHANGE
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T,cost;
+	cin >> T;
+
+while(T--){
+    cin >> cost;
+    cout << (100 - cost) << endl;
+}
+	return 0;
+}
