29ca97a2b4d9c364271c0ccb214bca17ca2bfe9a
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-30 22:29:01 +0530

    Update AMR15A.cpp

--- DAY_26/AMR15A.cpp
+++ DAY_26/AMR15A.cpp
@@ -3,12 +3,22 @@
 using namespace std;
 
 int main() {
-	int T,weapons;
+	int i,T,ready=0,not_ready=0;
 	cin >> T;
-
+    int arr[T];
+    
 while(T--){
-    cin >> weapons;
-    (weapons % 2 ==0) ? cout<<"READY FOR BATTLE"<<endl : cout<<"NOT READY"<<endl;
+        
+        cin >> arr[i];
+        if (arr[i]%2==0)
+            ready +=1;
+        else 
+            not_ready +=1;
+    
 }
+    (ready > not_ready) ? cout<<"READY FOR BATTLE"<<endl : cout<<"NOT READY"<<endl;
+    
+
 	return 0;
 }
+
