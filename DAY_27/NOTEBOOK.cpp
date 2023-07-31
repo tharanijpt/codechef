7d57a949ef070cebadf41b08ee711647b5c3ad43
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-31 21:39:48 +0530

    Create NOTEBOOK.cpp

---
+++ DAY_27/NOTEBOOK.cpp
@@ -0,0 +1,14 @@
+//https://www.codechef.com/problems/NOTEBOOK
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T,pulp;
+	cin >> T;
+
+while(T--){
+    cin >> pulp;
+    cout << (pulp*1000) /100 << endl;
+}
+	return 0;
+}
