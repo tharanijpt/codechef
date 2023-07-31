735742b94f485db44bc661146cccca86b0753cdd
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-07-31 21:40:04 +0530

    Create SUGARCANE.cpp

---
+++ DAY_27/SUGARCANE.cpp
@@ -0,0 +1,15 @@
+//https://www.codechef.com/problems/SUGARCANE
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T,sold,profit;
+	cin >> T;
+
+while(T--){
+    cin >> sold;
+    profit = (sold *50) *0.3;
+    cout <<profit << endl;
+}
+	return 0;
+}
