40544fe9ebfa78e224916a4023a4045b3d65c99f
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-08-01 21:45:14 +0530

    Create THREETOPICS.cpp

---
+++ DAY_28/THREETOPICS.cpp
@@ -0,0 +1,11 @@
+//https://www.codechef.com/problems/THREETOPICS
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,c,d;
+    cin >> a >> b >> c >> d;
+     (a==d || b==d || c==d  ) ? cout<<"YES"<<endl : cout<<"NO"<<endl;
+    
+	return 0;
+}
