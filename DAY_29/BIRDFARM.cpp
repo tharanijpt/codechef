d1154486b94995124a50446f9225085b80200758
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-08-02 21:58:36 +0530

    Update BIRDFARM.cpp

--- DAY_29/BIRDFARM.cpp
+++ DAY_29/BIRDFARM.cpp
@@ -1,21 +1,22 @@
-//https://www.codechef.com/problems/BIRDFARM
 #include <iostream>
 using namespace std;
 
 int main() {
-	int T,x,y,z;
-	cin >> T;
+    int T, x, y, z;
+    cin >> T;
 
-while(T--){
-    cin >> x >> y >> z;
-    if (x==y && z%x==0)
-    cout << "any" << endl;
-    else if (z % x == 0)
-    cout <<"chicken" << endl;
-    else if (z % y ==0)
-    cout <<"duck" << endl;
-    else 
-    cout <<"none" << endl;
-}
+    while (T--) {
+        
+	    cin>>x>>y>>z;
+	    if(z%x!=0 && z%y!=0)
+	        cout<<"NONE"<<endl;
+	    else if(z%x==0 && z%y==0)
+            cout<<"ANY"<<endl;
+	    else if(z%x==0)
+	        cout<<"CHICKEN"<<endl;
+	    else 
+	        cout<<"DUCK"<<endl;
+	    
+	}
 	return 0;
 }
