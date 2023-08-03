db24f0c047c0afd44a96d486a7c60bb078f02356
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-08-03 22:23:17 +0530

    Create MINNOO.cpp

---
+++ DAY_30/MINNOO.cpp
@@ -0,0 +1,16 @@
+//https://www.codechef.com/problems/MINNOO
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T,n;
+	cin >> T;
+
+while(T--){
+    cin >> n;
+
+    cout << n/2<< endl;
+
+}
+	return 0;
+}
