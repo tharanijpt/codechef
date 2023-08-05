d1fd5e5c83ae2464c9eb904909a2d290014cb9d5
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-08-05 22:30:18 +0530

    Create CS2023_GYM.cpp

---
+++ DAY_32/CS2023_GYM.cpp
@@ -0,0 +1,15 @@
+//https://www.codechef.com/problems/CS2023_GYM
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+
+while(T--){
+    int x,y;
+    cin >> x >> y;
+    cout << y/2 + 1 << endl;
+}
+	return 0;
+}
