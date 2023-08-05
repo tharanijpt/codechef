a9b6baa6c6a2e9411321b09400034ef8885c7646
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-08-05 23:16:51 +0530

    Create FLOW016.cpp

--- DAY_32/FLOW016.cpp
+++ DAY_32/FLOW016.cpp
@@ -1,16 +1,25 @@
 //https://www.codechef.com/problems/FLOW016
 #include <iostream>
-#include <algorithm>
-#include <numeric>
 using namespace std;
 
 int main() {
-	int T,a,b;
+	int T,a,b,i,j;
 	cin >> T;
 
 while(T--){
+int gcd;
+
 cin >> a >> b;
-    cout << __gcd(a,b) << " " << lcm(a,b) << endl;
+j=min(a,b);
+for (i=1;i<=j;i++){
+    if (a%i==0 && b%i==0)
+    gcd=i;
+    
+}
+    
+long int lcm =  (long int)a/(long int)gcd*b;
+
+    cout << gcd << " " << lcm << endl;
 }
 	return 0;
 }
