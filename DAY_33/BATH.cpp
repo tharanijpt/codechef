22d8a202937a57ec25646e0fad5581eae7d4d9db
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-08-06 21:55:52 +0530

    Create BATH.cpp

---
+++ DAY_33/BATH.cpp
@@ -0,0 +1,15 @@
+//https://www.codechef.com/problems/BATH
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+
+while(T--){
+int x,y;
+cin >> x >> y;
+cout << x/(y*2)<< endl;
+}
+	return 0;
+}
