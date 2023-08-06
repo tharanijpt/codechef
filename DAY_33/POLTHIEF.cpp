a6a44a44c3b3c8183fbdaecab74e345e9601e4aa
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-08-06 21:45:35 +0530

    Create POLTHIEF.cpp

---
+++ DAY_33/POLTHIEF.cpp
@@ -0,0 +1,15 @@
+//https://www.codechef.com/problems/POLTHIEF
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+
+while(T--){
+int x,y;
+cin >> x >>y;
+cout << abs( y-x)<< endl;
+}
+	return 0;
+}
