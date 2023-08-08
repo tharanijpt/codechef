75ad6526379e4895b634fafeabf3190d0d1b7dae
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-08-08 23:58:48 +0530

    Create TENPACKETS.cpp

---
+++ DAY_35/TENPACKETS.cpp
@@ -0,0 +1,15 @@
+//https://www.codechef.com/problems/TENPACKETS
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+
+while(T--){
+int a,b;
+cin >> a >>b ;
+cout << (2*b)+(1*a) << endl;
+}
+	return 0;
+}
