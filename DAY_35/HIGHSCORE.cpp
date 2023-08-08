2213905a3c158573044e3829408f041156625ce8
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-08-09 00:07:48 +0530

    Create HIGHSCORE.cpp

---
+++ DAY_35/HIGHSCORE.cpp
@@ -0,0 +1,16 @@
+//https://www.codechef.com/problems/HIGHSCORE
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+
+while(T--){
+int a,na,nb,nc,nd;
+cin >> a >> na >> nb >> nc >> nd;
+cout <<max({na,nb,nc,nd}) << endl;
+}
+	return 0;
+}
