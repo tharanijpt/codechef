f1a73bb361fe0d881fc0637b8cf3bf7d2a502eda
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-08-16 23:08:28 +0530

    Create FLOW013.cpp

---
+++ DAY_42/FLOW013.cpp
@@ -0,0 +1,18 @@
+//https://www.codechef.com/problems/FLOW013
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+
+while(T--){
+int a,b,c;
+cin >> a>>b >>c;
+if(a+b+c==180)
+cout <<"YES" << endl;
+else 
+cout << "NO"<< endl;
+}
+	return 0;
+}
