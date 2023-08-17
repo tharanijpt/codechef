e700af776891d406feb27039cd3b9d1327c6eafc
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-08-17 22:36:07 +0530

    Create FLOW018.cpp

---
+++ DAY_43/FLOW018.cpp
@@ -0,0 +1,19 @@
+//https://www.codechef.com/problems/FLOW018
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+
+while(T--){
+int n;
+cin >> n;
+int fact=1;
+for (int i=1;i<=n;i++){
+     fact = fact*i;
+}
+cout << fact<< endl;
+}
+	return 0;
+}
