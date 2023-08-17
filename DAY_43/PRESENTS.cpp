b300683b9657b674f3d705323fa94165c25fe261
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-08-17 22:21:20 +0530

    Create PRESENTS.cpp

---
+++ DAY_43/PRESENTS.cpp
@@ -0,0 +1,15 @@
+//https://www.codechef.com/problems/PRESENTS
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+
+while(T--){
+int a;
+cin >>a;
+cout << a-a/5<< endl;
+}
+	return 0;
+}
