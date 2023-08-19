64a7fe98ddb19e5caa765e02dbf8ff010f5c8fa7
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-08-19 19:55:56 +0530

    Create FLOW008.cpp

---
+++ DAY_44/FLOW008.cpp
@@ -0,0 +1,15 @@
+//https://www.codechef.com/problems/FLOW008
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+
+while(T--){
+int num;
+cin >> num;
+(num<10) ? cout<<"Thanks for helping Chef!"<<endl : cout<<"-1"<<endl;
+}
+	return 0;
+}
