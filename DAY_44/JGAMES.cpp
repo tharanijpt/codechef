787c3f1c55cd256c03022868c20149b090af8d94
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-08-19 19:14:08 +0530

    Create JGAMES.cpp

---
+++ DAY_44/JGAMES.cpp
@@ -0,0 +1,15 @@
+//https://www.codechef.com/problems/JGAMES
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+
+while(T--){
+int a,b;
+cin >> a>>b;
+((a+b)%2==0) ? cout<<"janmansh"<<endl : cout<<"jay"<<endl;
+}
+	return 0;
+}
