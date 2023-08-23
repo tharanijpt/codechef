1be9e4bb2a9d1b022ca6ad39da282a4b1f1606dd
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-08-24 00:38:00 +0530

    Create CUTPAPER.cpp

---
+++ DAY_48/CUTPAPER.cpp
@@ -0,0 +1,15 @@
+//https://www.codechef.com/problems/CUTPAPER
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+
+while(T--){
+int n,k;
+cin >> n >>k;
+cout <<(n/k)*(n/k)<< endl;
+}
+	return 0;
+}
