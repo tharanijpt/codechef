7cb7e91fe575a25a38975a80b4499f6882096f1d
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-08-26 00:04:43 +0530

    Create BOTTOM.cpp

---
+++ DAY_50/BOTTOM.cpp
@@ -0,0 +1,15 @@
+//https://www.codechef.com/problems/BOTTOM
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+
+while(T--){
+int num;
+cin >> num;
+cout <<7 - num << endl;
+}
+	return 0;
+}
