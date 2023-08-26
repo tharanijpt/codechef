f2a75f2711b49202daad7499c6470de3d272e913
tharanijpt <133520025+tharanijpt@users.noreply.github.com>
2023-08-26 23:21:11 +0530

    Create FRUITCHAAT.cpp

---
+++ DAY_51/FRUITCHAAT.cpp
@@ -0,0 +1,15 @@
+//https://www.codechef.com/problems/FRUITCHAAT
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+
+while(T--){
+int a,b;
+cin >> a >>b;
+cout << min((a/2),b)<< endl;
+}
+	return 0;
+}
