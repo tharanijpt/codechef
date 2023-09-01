93409995eedac172216fad46a60554c5ce827e91
Tharani Dharan <133520025+tharanijpt@users.noreply.github.com>
2023-09-01 11:45:33 +0530

    Create COCONUT.cpp

---
+++ DAY_57/COCONUT.cpp
@@ -0,0 +1,16 @@
+//https://www.codechef.com/problems/COCONUT
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+
+while(T--){
+int x,y,X,Y;
+cin >> x >> y >> X >> Y;
+int final = X/x + Y/y;
+cout <<final << endl;
+}
+	return 0;
+}
