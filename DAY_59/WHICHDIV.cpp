6a2bdac5097adf2fef8e35af5e5694ca3b5bd28a
Tharani Dharan <133520025+tharanijpt@users.noreply.github.com>
2023-09-03 23:03:21 +0530

    Create WHICHDIV.cpp

---
+++ DAY_59/WHICHDIV.cpp
@@ -0,0 +1,21 @@
+//https://www.codechef.com/problems/WHICHDIV
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+
+while(T--){
+int r;
+cin >> r;
+    if (r<1600)
+        cout <<"3" << endl;
+    else if (r>=2000 )
+        cout <<"1" << endl;
+    else 
+        cout <<"2" << endl;
+
+}
+	return 0;
+}
