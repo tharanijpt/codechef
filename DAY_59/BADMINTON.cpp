556fd78ba88f543b694699083c2c3ca8809056e8
Tharani Dharan <133520025+tharanijpt@users.noreply.github.com>
2023-09-03 23:13:48 +0530

    Create BADMINTON.cpp

---
+++ DAY_59/BADMINTON.cpp
@@ -0,0 +1,16 @@
+//https://www.codechef.com/problems/BADMINTON
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+
+while(T--){
+int p;
+cin >>p;
+    cout << (p/2)+1 << endl;
+}
+	return 0;
+}
