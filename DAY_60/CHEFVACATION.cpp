c427a49543c6836e183227a337331127e7c045b7
Tharani Dharan <133520025+tharanijpt@users.noreply.github.com>
2023-09-04 22:43:03 +0530

    Create CHEFVACATION.cpp

---
+++ DAY_60/CHEFVACATION.cpp
@@ -0,0 +1,15 @@
+//https://www.codechef.com/problems/CHEFVACATION
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+
+while(T--){
+int x,y,z;
+cin >> x >> y >>z;
+(x+y<=z) ? cout<<"YES"<<endl : cout<<"NO"<<endl;
+}
+	return 0;
+}
