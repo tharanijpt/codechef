920c7b9ac30c35f1fe6e767bd1b2981c05741eba
Tharani Dharan <133520025+tharanijpt@users.noreply.github.com>
2023-09-07 22:13:58 +0530

    Create LUCKYSEVEN.cpp

---
+++ DAY_63/LUCKYSEVEN.cpp
@@ -0,0 +1,15 @@
+//https://www.codechef.com/problems/LUCKYSEVEN
+#include <iostream>
+using namespace std;
+
+int main() {
+
+char str[10];
+for (int i=0;i<10;i++){
+    cin >> str[i];
+    if (i==6){
+        cout <<str[i] << endl;
+    }
+}
+	return 0;
+}
