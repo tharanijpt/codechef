12713d15ab38ef66a921d29054d9e5e18c3509dd
Tharani Dharan <133520025+tharanijpt@users.noreply.github.com>
2023-09-08 22:56:24 +0530

    Create EXISTENCE.cpp

---
+++ DAY_64/EXISTENCE.cpp
@@ -0,0 +1,19 @@
+//https://www.codechef.com/problems/EXISTENCE
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+
+while(T--){
+long long int a,b,r1,r2;
+cin >> a >>b;
+r1 = a*a;
+r2 = b*2;
+(r1==r2) ? cout<<"YES"<<endl : cout<<"NO"<<endl;
+
+}
+	return 0;
+}
