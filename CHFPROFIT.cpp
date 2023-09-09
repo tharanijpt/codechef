920599d92762e74d5ed1cac1b0418ad68893686d
Tharani Dharan <133520025+tharanijpt@users.noreply.github.com>
2023-09-09 22:19:58 +0530

    Create CHFPROFIT.cpp

---
+++ CHFPROFIT.cpp
@@ -0,0 +1,16 @@
+//https://www.codechef.com/problems/CHFPROFIT
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+
+while(T--){
+int x,y,z;
+cin >> x >>y >>z;
+int profit = x*z - x*y ;
+cout << profit << endl;
+}
+	return 0;
+}
