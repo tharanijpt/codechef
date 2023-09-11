638154fc2812f2afcab45aa9f63396e84f25838b
Tharani Dharan <133520025+tharanijpt@users.noreply.github.com>
2023-09-12 00:20:49 +0530

    Update PENALTY.cpp

--- DAY_67/PENALTY.cpp
+++ DAY_67/PENALTY.cpp
@@ -7,16 +7,16 @@ int main() {
 	cin >> T;
 
 while(T--){
-int t1[5];
-int t2[5];
+int t[11];
 int sum1=0,sum2=0;
-for (int i=0;i<5;i++){
-    cin >> t1[i];
-    sum1 += t1[i];
+for(int i=1;i<11;i++){
+    cin >> t[i];
 }
-for (int i=0;i<5;i++){
-    cin >> t2[i];
-    sum2 += t2[i];
+for (int i=1;i<10;i+=2){
+    sum1 += t[i];
+}
+for (int i=2;i<=10;i+=2){
+    sum2 += t[i];
 }
     if (sum2==sum1)
         cout <<"0" << endl;
