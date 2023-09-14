6b213c516e5364135bb1f8c4223ae08bd3f3b39f
Tharani Dharan <133520025+tharanijpt@users.noreply.github.com>
2023-09-14 20:39:47 +0530

    Create AIRINDEX.cpp

---
+++ DAY_69/AIRINDEX.cpp
@@ -0,0 +1,10 @@
+//https://www.codechef.com/problems/AIRINDEX
+#include <iostream>
+using namespace std;
+
+int main() {
+int a;
+cin >> a;
+(a<100) ? cout<<"YES"<<endl : cout<<"NO"<<endl;
+	return 0;
+}
