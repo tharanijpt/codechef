8ec6a70e30093c074efdad40be993e9cc5342209
Tharani Dharan <133520025+tharanijpt@users.noreply.github.com>
2023-09-26 22:01:21 +0530

    Create SHOEFIT.cpp

---
+++ DAY_79/SHOEFIT.cpp
@@ -0,0 +1,19 @@
+//https://www.codechef.com/problems/SHOEFIT
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+
+while(T--){
+int a,b,c;
+cin >> a >>b >> c;
+if (a==b && b==c)
+    cout <<0<< endl;
+else 
+    cout <<1<< endl;
+    
+}
+	return 0;
+}
