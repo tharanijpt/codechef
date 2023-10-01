5eb3c1b337fe29871b45653c5f7dfa663848be1f
Tharani Dharan <133520025+tharanijpt@users.noreply.github.com>
2023-10-01 21:25:28 +0530

    Create NEWSPAPER.cpp

---
+++ DAY_83/NEWSPAPER.cpp
@@ -0,0 +1,11 @@
+//https://www.codechef.com/problems/NEWSPAPER
+#include <iostream>
+using namespace std;
+
+int main() {
+int a;
+cin >> a;
+    (a==8 || a==9 || a==10) ? cout<<"YES"<<endl : cout<<"NO"<<endl;
+
+	return 0;
+}
