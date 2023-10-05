bb3bd09e68847b98a3a95a888909f14180c341dd
Tharani Dharan <133520025+tharanijpt@users.noreply.github.com>
2023-10-05 22:30:28 +0530

    Create GROUPASSGN.cpp

---
+++ DAY_86/GROUPASSGN.cpp
@@ -0,0 +1,15 @@
+//https://www.codechef.com/problems/GROUPASSGN
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+
+while(T--){
+int n,x;
+cin >> n >> x;
+cout <<2*n-(x-1) << endl;
+}
+	return 0;
+}
