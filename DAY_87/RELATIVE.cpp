21110aa6f8c4803ab9b29a35e9834caaf12cdf82
Tharani Dharan <133520025+tharanijpt@users.noreply.github.com>
2023-10-07 23:03:26 +0530

    Create RELATIVE.cpp

---
+++ DAY_87/RELATIVE.cpp
@@ -0,0 +1,15 @@
+//https://www.codechef.com/problems/RELATIVE
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+
+while(T--){
+int g,c;
+cin >> g >> c;
+cout <<(c*c)/(2*g) << endl;
+}
+	return 0;
+}
