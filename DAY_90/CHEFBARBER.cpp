959d584bd0a6ba587c093720c5810f9c981baaaa
Tharani Dharan <133520025+tharanijpt@users.noreply.github.com>
2023-10-11 23:44:52 +0530

    Create CHEFBARBER.cpp

---
+++ DAY_90/CHEFBARBER.cpp
@@ -0,0 +1,15 @@
+//https://www.codechef.com/problems/CHEFBARBER
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+
+while(T--){
+int n,m;
+cin >> n >>m;
+cout <<n*m << endl;
+}
+	return 0;
+}
