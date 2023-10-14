5de09ff774fad0536d3f3aa55b7f86754fa327f5
Tharani Dharan <133520025+tharanijpt@users.noreply.github.com>
2023-10-14 23:22:58 +0530

    Create PR0BLEM.cpp

---
+++ DAY_93/PR0BLEM.cpp
@@ -0,0 +1,18 @@
+//https://www.codechef.com/problems/PR0BLEM
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+
+while(T--){
+int a,b;
+cin >> a>>b;
+if ((a-b)%2 ==0)
+    cout <<"yes" << endl;
+else 
+    cout <<"no" << endl;
+}
+	return 0;
+}
