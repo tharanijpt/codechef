6a69eb78469a9c01d5356b1b6c747888fcdd13da
Tharani Dharan <133520025+tharanijpt@users.noreply.github.com>
2023-10-15 22:28:59 +0530

    Create RATING.cpp

---
+++ DAY_94/RATING.cpp
@@ -0,0 +1,15 @@
+//https://www.codechef.com/problems/RATING
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+
+while(T--){
+int s;
+cin >>s;
+cout << -s-1<< endl;
+}
+	return 0;
+}
