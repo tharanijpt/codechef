c01d66feb6940dcb6a5219eefbd33e251eec01c2
Tharani Dharan <133520025+tharanijpt@users.noreply.github.com>
2023-10-16 22:27:37 +0530

    Create UWCOI21A.cpp

---
+++ DAY_95/UWCOI21A.cpp
@@ -0,0 +1,15 @@
+//https://www.codechef.com/problems/UWCOI21A
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+
+while(T--){
+int  n;
+cin >>n;
+cout <<1<<" "<<n << endl;
+}
+	return 0;
+}
